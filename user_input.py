3e65959826aa87a108d4a385cee2dce5cd20d918
kimani3 <anomallymi15@gmail.com>
2024-11-07 14:36:14 +0300

    user input

---
+++ user_input.py
@@ -0,0 +1,6 @@
+
+name = input('enter your name here: ')
+age = input('Enter your age: ')
+location = input('Enter your current location: ')
+
+print('Hello ' +name, 'you are ' +age, 'years old and live in ' +location)
